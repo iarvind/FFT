7f70a84f7aa08411292e29ea4fe6cfe285739cfd
Arvind Choudhary <iarvind@live.com>
2014-04-06 02:39:55 +0530

    repaired interpolation with both polynomials with diff bounds

--- main.cpp
+++ main.cpp
@@ -9,47 +9,48 @@ using namespace std;
 class fft
 {
 private:
+	int na,nb;
 	int new_n;
-	float t_simple;
 	float t_fft;
-	vector <int> A;
-	vector <int> B;
+	vector <double> A;
+	vector <double> B;
 	vector <double> C;
+	vector <complex <double> >C_Pointwise;
 public:
-	fft(int &);
-	void setupDegree(int &);
-	void simpleMultiplication(int &);
-	vector< complex<double> > DFT (vector <int> &);
-	void interpolation (vector <complex <double> > &);
-	void fastFourierTransformation(int &);
+	fft(int &, int &);
+	void setupDegree();
+	void finalise();
+	vector< complex<double> > DFT (vector <double> &);
+	vector< complex<double> > interpolation (vector< complex<double> > &);
+	void fastFourierTransformation();
 
 };
 
-fft::fft(int &n)
+fft::fft(int &n1, int &n2)
 {
+	na=n1;
+	nb=n2;
 	int k;				//temp. condition
 	cout<<"Enter coeff. of A(x) (starting from lower order including 0): \n";
-	for(int i = 0; i<n; i++)
+	for(int i = 0; i<na; i++)
 	{
 		cin>>k;
 		A.push_back(k);
 	}
 
 	cout<<"Enter coeff. of B(x): \n";
-	for(int i = 0; i<n; i++)
+	for(int i = 0; i<nb; i++)
 	{
 		cin>>k;
 		B.push_back(k);
 	}
-
-	setupDegree(n);
 	return;
 }
 
-void fft::setupDegree(int &n)
+void fft::setupDegree()
 {
 	int i=0;
-	int k = ((2*n)-1);
+	int k = (na+nb-1);
 	while(1)
 	{
 		if(pow(2,i)>=k)
@@ -60,34 +61,7 @@ void fft::setupDegree(int &n)
 	return;
 }
 
-void fft::simpleMultiplication(int &n)
-{
-	int temp=0;
-	clock_t t;
-	t = clock();
-	for(int i = 0; i<(2*n - 1); i++)			//(2n-1) is the degree bound here!
-	{
-		for(int j = 0; j<=i; j++)
-		{
-			if(j<n && (i-j)<n)
-				temp += A[j]*B[i-j];
-		}
-		C.push_back(temp);
-		temp = 0;
-	}
-	t = clock() - t;
-	t_simple = ((float)t)/CLOCKS_PER_SEC;
-		#ifdef DEBUG
-		cout<<"MULTI_SIMPLE"<<endl;
-		for(int i = 0; i<(2*n - 1); i++)
-			cout<<C[i]<<" ";
-        cout<<endl;
-		#endif
-	C.resize(0);
-	return;
-}
-
-vector < complex<double> > fft::DFT (vector <int> &V)
+vector < complex<double> > fft::DFT (vector <double> &V)
 {
 	int n = V.size();
 
@@ -109,8 +83,8 @@ vector < complex<double> > fft::DFT (vector <int> &V)
 		vector < complex<double> > y (n) ;
 
 
-		vector < int > a1 ;
-		vector < int > a2 ;
+		vector < double > a1 ;
+		vector < double > a2 ;
 
 		for (int i = 0; i < n; i+=2)
 		{
@@ -131,72 +105,107 @@ vector < complex<double> > fft::DFT (vector <int> &V)
     }
 }
 
-void fft::interpolation (vector <complex <double> > &CP)
+
+vector< complex<double> > fft::interpolation (vector< complex<double> > &V)
 {
-	complex <double> omega_n (cos(2*PI/new_n),sin(2*PI/new_n));
-	vector < complex <double> > temp;
-	for (int i = 0; i < new_n; ++i)
+	int n = V.size();
+
+	if(n==1)
 	{
-	    complex <double> k (0,0);
-		for (int j = 0; j < new_n; ++j)
-		{
-			k += (CP[j]*(pow(omega_n,-(i*j))));
-		}
-		C.push_back(k.real()/new_n);
+		vector <complex <double> >R;
+		complex <double> temp (real(V[0]),imag(V[0]));
+		R.push_back(temp);
+		return R;
 	}
+
+	else
+    {
+      complex <double> omega_n (cos(2*PI/n),-sin(2*PI/n));				//w_n
+		complex <double> omega (1,0);												//w=1
+
+		vector < complex<double> > v1 ;
+		vector < complex<double> > v2 ;
+		vector < complex<double> > y (n) ;
+
+
+		vector < complex<double> > a1 ;
+		vector < complex<double> > a2 ;
+
+		for (int i = 0; i < n; i+=2)
+		{
+			a1.push_back(V[i]);
+			a2.push_back(V[i+1]);
+		}
+
+		v1 = interpolation(a1);
+		v2 = interpolation(a2);
+
+		for (int i = 0; i < (n/2); ++i)
+		{
+			y[i] = (v1[i] + omega*v2[i]);
+			y[i+(n/2)] = (v1[i] - omega*v2[i]);
+			omega *= omega_n;
+		}
+		return y;
+    }
 }
 
-void fft::fastFourierTransformation(int &n)
+void fft::finalise()
+{
+	for (int i=0; i<(na + nb - 1); i++)
+		C.push_back ( real(C_Pointwise[i] ) / new_n);
+	return;
+}
+
+void fft::fastFourierTransformation()
 {
 	clock_t t;
 	t = clock();
+	setupDegree();
 	vector <complex <double> >Y1;
 	vector <complex <double> >Y2;
+
+	//Adding zeros for rest of the term till it becomes closest 2^n
 	while(A.size() < new_n)
 		A.push_back(0);
 	while(B.size() < new_n)
 		B.push_back(0);
-    cout<<endl;
+
+	//Evaluation
 	Y1 = DFT(A);
 	Y2 = DFT(B);
-
-										//Multiplication [step 3]
-	vector <complex <double> >C_Pointwise;
+	
+	
+	//Point wise multiplication
 	for (int i = 0; i < new_n; ++i)
 		C_Pointwise.push_back(Y1[i]*Y2[i]);
 
-	interpolation(C_Pointwise);
+	//Interpolation
+	C_Pointwise = interpolation(C_Pointwise);
+	finalise();
 
 	t = clock() - t;
 	t_fft = ((float)t)/CLOCKS_PER_SEC;
 
-	#ifdef
 	cout<<"MULTI_FFT"<<endl;
-	for (int i=0; i<(2*n - 1); i++)
+	for (int i=0; i<(na + nb - 1); i++)
         cout<<C[i]<<" ";
     cout<<endl;
-    #endif
-    cout<<t_simple<<" "<<t_fft<<endl;
-
-
+    cout<<"Time: "<<t_fft<<endl;
 
 	#ifdef DEBUG
 	cout<<"check\n";
 	for (int i = 0; i < Y1.size(); ++i)
 		cout<<Y1[i]<<" "<<Y2[i]<<endl;
 	#endif
-
 }
-
-
 
 int main()
 {
-	int n;
-	cout<<"degree bound?\n";
-		cin>>n;
-	fft f(n);
-	f.simpleMultiplication(n);
-	f.fastFourierTransformation(n);
+	int na,nb;
+	cout<<"degree bound of A and B respectively?\n";
+		cin>>na>>nb;
+	fft f(na, nb);
+	f.fastFourierTransformation();
 	return 0;
 }
